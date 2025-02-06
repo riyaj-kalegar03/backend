a0768734813cc253e04c5eeaa01d664e915d2925
riyajkalegar <work.riyajkalegar@gmail.com>
2025-02-06 13:55:17 +0530

    update

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://riyajkalegar:w6Fuwt0hKlxIoHBW@cluster0.v12h8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+  jwtSecret: "your_jwt_secret",
+  jwtExpiresIn: "1h",
+};
